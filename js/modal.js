a13430b275fbf673111259baa7cab9542b930ef9
Alexander Krupka <bamaboy483@gmail.com>
2022-12-21 20:54:47 +0200

    req

--- js/modal.js
+++ js/modal.js
@@ -1,28 +1,74 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-modal-open]"),
+//     modal: document.querySelector("[data-modal]"),
+//   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.modal.addEventListener("click", onBackdropClick);
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.modal.addEventListener("click", onBackdropClick);
 
-  function onKeydown(e) {
-    if (e.code === "Escape") {
-      toggleModal();
-      window.removeEventListener("keydown", onKeydown);
+//   function onKeydown(e) {
+//     if (e.code === "Escape") {
+//       toggleModal();
+//       window.removeEventListener("keydown", onKeydown);
+//     }
+//   }
+
+//   function onBackdropClick(e) {
+//     if (e.currentTarget === e.target) {
+//       toggleModal();
+//     }
+//   }
+
+//   function toggleModal() {
+//     document.body.classList.toggle("modal-open");
+//     refs.modal.classList.toggle("is-hidden");
+//     window.addEventListener("keydown", onKeydown);
+//   }
+// })();
+
+const modalLinks = document.querySelectorAll(".modal-link");
+const body = document.querySelector("body");
+const lockPadding = document.querySelectorAll(".lock-padding");
+
+let unlock = true;
+
+const timeout = 250;
+
+if (modalLinks.length > 0) {
+  for (let index = 0; index < modalLinks.length; index++) {
+    const modalLink = modalLinks[index];
+    modalLink.addEventListener("click", function (e) {
+      const modalName = modalLink.getAttribute("href").replace("#", "");
+      const currentModal = document.getElementById(modalName);
+      modalOpen(currentModal);
+      e.preventDefault();
+    });
+  }
+}
+
+function modalOpen(currentModal) {
+  if (currentModal && unlock) {
+    const modalActive = document.querySelector(".modal.open");
+    if (modalActive) {
+      modalClose(modalActive, false);
+    } else {
+      bodyLock();
+    }
+    currentModal.classList.add(".open");
+    currentModal.addEventListener("click", function (e) {
+      if (!e.target.closest(".modal__content")) {
+        modalClose(e.target.closest(".modal"));
+      }
+    });
+  }
+}
+
+function modalClose(modalActive, doUnlock = true) {
+  if (unlock) {
+    modalActive.classList.remove("open");
+    if (doUnlock) {
+      bodyLock();
     }
   }
-
-  function onBackdropClick(e) {
-    if (e.currentTarget === e.target) {
-      toggleModal();
-    }
-  }
-
-  function toggleModal() {
-    document.body.classList.toggle("modal-open");
-    refs.modal.classList.toggle("is-hidden");
-    window.addEventListener("keydown", onKeydown);
-  }
-})();
+}
